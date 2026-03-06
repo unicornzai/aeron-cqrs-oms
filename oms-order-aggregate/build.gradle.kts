4ce255969ef3034922d398462036795382951941
dental-dash <dental.dash.app@gmail.com>
2026-03-06 10:36:12 +0000

    Removing oms-order-aggregate project

--- oms-order-aggregate/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-dependencies {
-    implementation(project(":oms-common"))
-    implementation(project(":oms-fix-client-gateway:fix-common"))  // for PlaceOrderCommand (templateId=20)
-    implementation(libs.aeron.client)
-    implementation(libs.aeron.archive)
-}
